2620c1099922ccd99258c005fedb7cb74137c3b5
dhanya180 <cs23b055@iittp.ac.in>
2024-11-20 21:33:26 +0530

    Update publish-form.component.jsx

--- src/components/publish-form.component.jsx
+++ src/components/publish-form.component.jsx
@@ -105,7 +105,7 @@ const PublishForm = () => {
       content,  
       draft: false
     };
-    let pranathiURL = "https://animimic-server-3.onrender.com";
+    let pranathiURL = "https://animimic-server-6.onrender.com";
     axios.post(pranathiURL+"/create-blog",blogObj,
         )
         .then(()=>{
